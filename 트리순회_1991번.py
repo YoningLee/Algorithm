d2738f7cca0545cb451bc65e47e1a45e26355176
Lee YongIn <67788699+YoningLee@users.noreply.github.com>
2021-02-06 23:50:45 +0900

    Update 트리순회_1991번.py

--- 트리순회_1991번.py
+++ 트리순회_1991번.py
@@ -1,11 +1,50 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+class Queue:
+    def __init__(self):
+        self.data=[]
+
+class BinaryTree:
+    def __init__(self):
+        self.root = Node(None)
+    def insert(self, root, left, right):
+        self.current_node = self.root
+        depth=0
+        while True:
+            if(self.current_node.value == None):
+                self.current_node.value = root
+                self.current_node.left = left
+                self.current_node.right = right
+                break
+            elif((self.current_node.left.value != root)and(self.current_node.right.value != root)):
+                #이진트리에 root과 같은 값을 가진 노드 찾기
+
+            else:
+                if(self.current_node.left.value == root):
+                    self.current_node = self.current_node.left
+                    self.current_node.left = left
+                    self.current_node.right = right
+                    break
+                elif(self.current_node.right.value == root):
+                    self.current_node = self.current_node.right
+                    self.current_node.left = left
+                    self.current_node.right = right
+                    break
+    def search(self, value):
+        self.current_node = self.root
+
+
 num = int(input())
-tree={}
+tree = []
 for i in range(num):
-    x, y, z = map(str, input().split())
-    if y == '.':
-        y = None
-    if z == '.':
-        z = None
-    tree[x] = list((y,z))
-tree_copy = tree
-print(tree)
+    list = []
+    list = (str(x) for x in input().split())
+    tree.append(list)
+Q=[]Queue
+BT = BinaryTree()
+for i in range(num):
+    BT.insert(tree[i][0], tree[i][1], tree[i][2])
+첫 번째 목표: 이진트리 생성하기
